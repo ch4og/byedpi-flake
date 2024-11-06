5d28fc2bf7c95dfaba200dead7e3b5eb84deb94f
ch4og <mitanick@ya.ru>
2024-11-06 19:50:12 +0300

    update to 0.15 and support other archs

--- flake.nix
+++ flake.nix
@@ -1,30 +1,38 @@
 {
   description = "byedpi flake";
 
-  inputs = {nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs = {
-    self,
     nixpkgs,
-  }: {
-    packages.x86_64-linux.byedpi = with nixpkgs.legacyPackages.x86_64-linux;
-      stdenv.mkDerivation rec {
-        name = "ciadpi";
-        version = "0.14.1";
-        src = fetchFromGitHub {
-          owner = "hufrea";
-          repo = "byedpi";
-          rev = "v${version}";
-          sha256 = "sha256-JdL+3ETNxaEtOLUhgLSABL9C8w/EM4Ay37OXU5jLCFA=";
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages = with pkgs; {
+          byedpi = stdenv.mkDerivation rec {
+            name = "ciadpi";
+            version = "0.15";
+            src = fetchFromGitHub {
+              owner = "hufrea";
+              repo = "byedpi";
+              rev = "v${version}";
+              sha256 = "sha256-jsQFfIvWvpJRBewUSS5bhDGocfPTvtG6LiejUxdqk28=";
+            };
+            installPhase = ''
+              runHook preInstall
+              install -Dm755 ciadpi $out/bin/ciadpi
+              runHook postInstall
+            '';
+          };
+          default = byedpi;
         };
-        buildInputs = [gcc gnumake];
-        buildPhase = ''
-          make
-        '';
-        installPhase = ''
-          mkdir -p $out/bin
-          cp ciadpi $out/bin/
-        '';
-      };
-  };
+      }
+    );
 }
